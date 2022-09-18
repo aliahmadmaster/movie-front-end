ca5223ea810d32d076c4f02dd54d0f8e03e67f9b
aliahmadmaster <aliahmadmaster@gmail.com>
2022-09-18 03:05:50 -0700

    auth-working

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/booker'),
+      dir: require('path').join(__dirname, './coverage/movie-front-end'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'booker';
+  title = 'movie-front-end';
 }
